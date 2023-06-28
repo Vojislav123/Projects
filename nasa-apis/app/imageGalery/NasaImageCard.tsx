1fa74bc24fe65574aaad02097f8482fbf9e12557
Vojislav123 <vojislavalic@hotmail.com>
2023-06-28 21:58:40 +0200

    A completed project using multiple API's provided by Nasa

---
+++ nasa-apis/app/imageGalery/NasaImageCard.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+
+import nasaLogo from '@/service/nasaLogo.png';
+import ImagePopup from '@/service/imagePopup';
+
+interface NasaImage {
+  data: {
+    nasa_id: string;
+    title: string;
+  }[];
+  links: {
+    href: string;
+  }[];
+}
+
+const NasaImageCard: React.FC<{ image: NasaImage }> = ({ image }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    simulateLoading(image).then(() => {
+      setLoading(false);
+    });
+  }, [image]);
+
+  const simulateLoading = (image: NasaImage): Promise<NasaImage> => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(image);
+      }, 2000);
+    });
+  };
+
+  if (loading) {
+    return (
+      <div className="justify-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        <img src={nasaLogo.src} alt="NASA Placeholder" />
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return <ImagePopup image={image} />;
+};
+
+export default NasaImageCard;
---
+++ nasa-apis/app/page.tsx
@@ -0,0 +1,12 @@
+
+const homePage = () => {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold mb-10">
+        Welcome to my first project in Next.js
+      </h1>
+    </main>
+  );
+};
+
+export default homePage;
